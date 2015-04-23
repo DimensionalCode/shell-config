45d3ceab4867785fb464026d983ef38b77b7b685
Noel Bhattacharyya <bhattnoe@umich.edu>
2015-04-23 12:59:28 -0400

    Update

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cp .* ~/
 touch ~/.localrc
 
 git clone git://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_basic_vimrc.sh
+sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 cd ~/shell-config
 cp my_configs.vim ~/.vim_runtime/
