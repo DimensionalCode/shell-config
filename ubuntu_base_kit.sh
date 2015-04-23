fce83740f83d6cea96070516f24fb05dd0a3bf5f
DimensionalCode <noel.bhattacharyya@gmail.com>
2015-04-23 13:10:02 -0400

    Bash Kit

---
+++ ubuntu_base_kit.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y install gparted smartmontools gsmartcontrol clamav clamtk playonlinux synaptic gddrescue chromium-browser hardinfo gpart pv exfat-fuse exfat-utils hfsplus hfsprogs vim testdisk parted fcrackzip pdfcrack john aircrack-ng
