e203cf58904254dc50074fffae255fd06d0f2de9
Noel Bhattacharyya <DimensionalCode@users.noreply.github.com>
2022-09-13 20:20:54 -0400

    Clone with https

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ cd ~/shell-config
 cp .* ~/
 touch ~/.localrc
 
-git clone git://github.com/amix/vimrc.git ~/.vim_runtime
+git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 cd ~/shell-config
 cp my_configs.vim ~/.vim_runtime/
 
-git clone git://github.com/vim-scripts/Conque-GDB.git ~/.vim_runtime/sources_non_forked/Conque-GDB
+git clone https://github.com/vim-scripts/Conque-GDB.git ~/.vim_runtime/sources_non_forked/Conque-GDB
 
 if grep -Fq "collections.abc" ~/.vim_runtime/sources_non_forked/Conque-GDB/autoload/conque_gdb/conque_gdb.py
 then
